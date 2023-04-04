7f76ec1d269204d59ece6cd9bb3cc5588553ad4e
Shivam Tiwari <tshivam.iitkgp@gmail.com>
2023-04-05 00:45:52 +0530

    adding codebase to github

---
+++ src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,19 @@
+package com.example.employeemanager;
+
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+}
